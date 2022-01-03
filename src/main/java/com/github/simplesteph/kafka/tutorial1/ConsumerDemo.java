22dc58f984fb5ecc3e758139fab201e1edd42e86
varadarajanvignesh <varadarajanvignesh@gmail.com>
2022-01-03 16:53:54 +0530

    Learn

--- src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemo.java
+++ src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemo.java
@@ -1,6 +1,8 @@
 package com.github.simplesteph.kafka.tutorial1;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -23,11 +25,11 @@ public class ConsumerDemo {
 
         // Create Consumer Config
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapservers);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapservers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         //create consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
@@ -36,8 +38,13 @@ public class ConsumerDemo {
         consumer.subscribe(Arrays.asList("first_topic"));
 
         //poll for new data
-        while(true){
-            consumer.poll(Duration.ofMillis(100));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Parition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
     }
 }
-
